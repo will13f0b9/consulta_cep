9c0404fe291286af9d4417939be35f308ec8f384
William <william_rocha@hotmail.com>
2018-02-16 22:14:43 -0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,25 @@
 console.log("USANDO O GULP");
+
+var gulp = require('gulp');
+var uglifycss = require('gulp-uglifycss');
+
+gulp.task('jsmin',function(){
+    console.log("Minificando o js");
+});
+
+gulp.task('cssmin',function(){
+    console.log("Minificando o css");
+
+    gulp
+        .src('../src/css/*.css')
+        .pipe(uglifycss());
+});
+
+gulp.task('htmlmin',function(){
+    console.log("Minificando o html");
+});
+
+gulp.task('default', ['jsmin','cssmin', 'htmlmin']);
+
+
+console.log(gulp);
